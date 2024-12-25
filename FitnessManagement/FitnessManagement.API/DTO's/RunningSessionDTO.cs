f71894b3ccefd7a921ba48417506d8d115b6e3c7
RabaeySteve <steve.rabaey@student.hogent.be>
2024-12-25 22:17:25 +0100

    foutje gemaakt

---
+++ FitnessManagement/FitnessManagement.API/DTO's/RunningSessionDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace FitnessManagement.API.DTO_s {
+    public class RunningSessionDTO {
+        public int RunningSessionId { get; set; } 
+        public int MemberId { get; set; }
+        public DateTime Date { get; set; } 
+        public int Duration { get; set; } 
+        public double AvgSpeed { get; set; } 
+        public List<RunningSessionDetailDTO> Details { get; set; } 
+    }
+
+    public class RunningSessionDetailDTO {
+        public int SeqNr { get; set; } 
+        public int IntervalTime { get; set; } 
+        public double IntervalSpeed { get; set; } 
+    }
+
+}
--- FitnessManagement/FitnessManagement.API/obj/Debug/net8.0/FitnessManagement.API.AssemblyInfo.cs
+++ FitnessManagement/FitnessManagement.API/obj/Debug/net8.0/FitnessManagement.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitnessManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2524846bbce9d8aa4f2afd38fb6983bd03cdead4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fefd78c7f256fb376b6adbab9d875d1f03f5ee6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitnessManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitnessManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
